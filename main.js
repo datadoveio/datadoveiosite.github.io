b94dc9191a3d5dc0e2b718fdcd2e6e8cc6db1b99
datadoveio <65378119+datadoveio@users.noreply.github.com>
2020-05-25 01:15:11 -0400

    removed pw

--- main.js
+++ main.js
@@ -7,34 +7,35 @@ $(window).on("load",function(){
         overflow: "unset"
     });
     $(".loader").fadeOut("slow");
+	console.log("0525");
 });
 
 {
-    var password = [
-        "jUHzEQ9gASAc",
-        "4cx7avXLhNXE",
-        "2f8T8TRZe3aU",
-        "2FqBhXk64sLt"
-    ];
-    var userInput = "";
-    document.querySelector(".form__box").addEventListener("input", function(event){
-        console.log(event.target.value);
-        userInput = event.target.value;
+//     var password = [
+//         "jUHzEQ9gASAc",
+//         "4cx7avXLhNXE",
+//         "2f8T8TRZe3aU",
+//         "2FqBhXk64sLt"
+//     ];
+//     var userInput = "";
+//     document.querySelector(".form__box").addEventListener("input", function(event){
+//         console.log(event.target.value);
+//         userInput = event.target.value;
 
 
-    });
-    document.addEventListener("keydown", function(event){
+//     });
+//     document.addEventListener("keydown", function(event){
             
-        if(event.keyCode == 13){
-            for(var i = 0; i < password.length; i++){
-                if(password[i] == userInput){
-                    document.querySelector(".twitter__button").classList.remove("hidden");
-                    document.querySelector(".form__box").classList.add("hidden");
-                    break;
-                }
-            }   
-        }
-    });
+//         if(event.keyCode == 13){
+//             for(var i = 0; i < password.length; i++){
+//                 if(password[i] == userInput){
+//                     document.querySelector(".twitter__button").classList.remove("hidden");
+//                     document.querySelector(".form__box").classList.add("hidden");
+//                     break;
+//                 }
+//             }   
+//         }
+//     });
     document.querySelector(".down__arrow").addEventListener("click", function(event){
         goTo = $(".features");
         var top = goTo.offset().top;
